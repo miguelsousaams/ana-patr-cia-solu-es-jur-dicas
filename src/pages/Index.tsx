38f7ae5e30948580b8f6d19d38f83032d390f876
AMS | Sports Studio <ams.sports.studio@gmail.com>
2026-02-11 08:47:17 +0000

    remove trust strip from scroll reveal

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -19,8 +19,6 @@ const Index = () => {
       <main>
         <ScrollReveal>
           <Hero />
-        </ScrollReveal>
-        <ScrollReveal>
           <TrustStrip />
         </ScrollReveal>
         <ScrollReveal>
